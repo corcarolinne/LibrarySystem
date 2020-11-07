e82056b08d2a21075aa574a3428c762b56e02275
Carolinne Cordeiro <carolinnemcordeiro@gmail.com>
2020-11-07 17:47:28 +0000

    reading first user's input
    reading user's chosen option from menu

--- cli.js
+++ cli.js
@@ -2,19 +2,30 @@
 
 // function to offer menu options to the user
 function displayOptionsMenu() {
-    console.log("Welcome to the library system!");
-    console.log("Select an option from the following menu:" + "\n" +
-    "1 - Search for a book" + "\n" +
-    "2 - Search for a reader" + "\n" +
-    "3 - Display books list" + "\n" +
-    "4 - Display readers list" + "\n" +
-    "5 - Borrow a book" + "\n" +
-    "6 - Return a book" + "\n" +
-    "7 - Display books in transit" + "\n" +
-    "8 - Exit"
+    console.log("Select an option from the following menu and press enter:" + "\n" +
+        "1 - Search for a book" + "\n" +
+        "2 - Search for a reader" + "\n" +
+        "3 - Display books list" + "\n" +
+        "4 - Display readers list" + "\n" +
+        "5 - Borrow a book" + "\n" +
+        "6 - Return a book" + "\n" +
+        "7 - Display books in transit" + "\n" +
+        "8 - Exit"
     );
+    return;
 }
-
-
-
 displayOptionsMenu();
+
+function readSelectedOption() {
+    // reading user's answer from menu
+    const readline = require('readline').createInterface({
+        input: process.stdin,
+        output: process.stdout
+    });
+    
+    readline.question("", optionNumber => {
+        console.log(`Choosen option was ${optionNumber}`);
+        readline.close();
+    })
+}
+readSelectedOption();
