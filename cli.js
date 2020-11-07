9d2f2e0d3f298ac4cd9f681614020d2b6c439e49
Carolinne Cordeiro <carolinnemcordeiro@gmail.com>
2020-11-07 17:19:35 +0000

    creating function to display system options

---
+++ cli.js
@@ -0,0 +1,20 @@
+// command line interface
+
+// function to offer menu options to the user
+function displayOptionsMenu() {
+    console.log("Welcome to the library system!");
+    console.log("Select an option from the following menu:" + "\n" +
+    "1 - Search for a book" + "\n" +
+    "2 - Search for a reader" + "\n" +
+    "3 - Display books list" + "\n" +
+    "4 - Display readers list" + "\n" +
+    "5 - Borrow a book" + "\n" +
+    "6 - Return a book" + "\n" +
+    "7 - Display books in transit" + "\n" +
+    "8 - Exit"
+    );
+}
+
+
+
+displayOptionsMenu();
