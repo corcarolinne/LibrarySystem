bffd107a29470309f0cfbb267e9b4d790e1c1c6b
corcarolinne <carolinnemcordeiro@gmail.com>
2020-11-30 21:33:14 +0000

    finished borrowing feature

--- operations/registerBookBorrowed.js
+++ operations/registerBookBorrowed.js
@@ -8,33 +8,53 @@ module.exports = registerBookBorrowed = (bookId, readerId) => {
 }
 
 function borrowBook(bookId, readerId) {
-    // declating array to hold borrowings data
+    // declarating array to hold borrowings data
     let borrowingsInFile = [];
+    // declarating obj to hold books data
+    let booksInFile = {};
 
     // Defining new borrow 
     let borrowing = { 
         bookId: `${bookId}`, 
         readerId: `${readerId}` 
     }; 
-        // Read borrowings.json file 
-    fs.readFile("borrowings.json", function(err, data) { 
+
+    // Read books.json file 
+    fs.readFile("./books/books.json", function(err, data) { 
         
         // Check for errors 
         if (err) throw err; 
     
-        // Converting to JSON borrowings already recorded
-        borrowingsInFile = JSON.parse(data);
+        // Converting to JSON books file
+        booksInFile = JSON.parse(data);
 
-        // including new borrowing in the array
-        borrowingsInFile.push(borrowing);
+        if (booksInFile[bookId].status === 'IN_TRANSIT') {
+            console.log('book not available')
+        } else {
+            booksInFile[bookId].status = 'IN_TRANSIT'
+            // write on books.json the books
+            fs.writeFile("./books/books.json", JSON.stringify(booksInFile, null, "  "), () => {});
 
-        fs.writeFile('borrowings.json', JSON.stringify(borrowingsInFile), err => {
-            console.log(borrowingsInFile);
-         
-            // Checking for errors 
-            if (err) throw err;  
-           
-            console.log("Done writing"); // Success 
-        });
-    });   
+            // Read borrowings.json file
+            fs.readFile("borrowings.json", function(err, data) { 
+            
+                // Check for errors 
+                if (err) throw err; 
+            
+                // Converting to JSON borrowings already recorded
+                borrowingsInFile = JSON.parse(data);
+
+                // including new borrowing in the array
+                borrowingsInFile.push(borrowing);
+
+                fs.writeFile('borrowings.json', JSON.stringify(borrowingsInFile, null, "  "), err => {
+                
+                    // Checking for errors 
+                    if (err) throw err;  
+                    
+                    console.log('book ' + bookId + ' borrowed sucessfully by reader ' + readerId)
+                });
+            })
+        }
+    });
 }
